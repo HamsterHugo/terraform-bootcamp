fdac75dafc9263f950e97f834f8f004c406f30ee
Benedict Meinke <Benedict_Meinke@web.de>
2026-07-15 16:19:43 +0200

    [ADD] Provider information.

--- main.tf
+++ main.tf
@@ -1,3 +1,20 @@
 terraform {
-    required_version = ">=1.5"
+  required_version = ">=1.6"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "lab_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "Terraform-Lab_VPC"
+  }
 }
