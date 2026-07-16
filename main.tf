849915b80e5a252f1c251f0694ea4e9785213a72
Benedict Meinke <Benedict_Meinke@web.de>
2026-07-16 14:15:46 +0200

    [ADD] resource public subnet.

--- main.tf
+++ main.tf
@@ -18,3 +18,13 @@ resource "aws_vpc" "lab_vpc" {
     Name = "Terraform-Lab_VPC"
   }
 }
+
+resource "aws_subnet" "public_subnet" {
+    vpc_id = aws_vpc.lab_vpc.id
+    cidr_block = "10.0.1.0/24"
+    availability_zone = "us-east-1a"
+    map_public_ip_on_launch = true
+    tags = {
+        Name = "Terraform-Public-Subnet"
+    }
+}
